557859b0c21067952e0247ff657ae913f899f858
xiechaoqiang <2481086105@qq.com>
2024-02-24 14:35:11 +0800

    完成basic paxos并进行单个提案者的单元测试

--- basic_paxos_test.go
+++ basic_paxos_test.go
@@ -53,3 +53,34 @@ func TestSingleProposer(t *testing.T) {
 		t.Errorf("learnValue = %s,excepted %s", learnValue, "hello world")
 	}
 }
+
+func TestTwoProposers(t *testing.T) {
+	// 1001,1002,1003是接受者id
+	acceptorIds := []int{1001, 1002, 1003}
+	// 2001是学习者id
+	learnerIds := []int{2001}
+	acceptors, learns := start(acceptorIds, learnerIds)
+	defer cleanup(acceptors, learns)
+
+	// 1，2是提议者id
+	p1 := &Proposer{
+		id:        1,
+		acceptors: acceptorIds,
+	}
+	v1 := p1.propose("hello world")
+
+	p2 := &Proposer{
+		id:        2,
+		acceptors: acceptorIds,
+	}
+	v2 := p2.propose("hello book")
+	if v1 != v2 {
+		t.Errorf("value1 = %s,value 2 = %s", v1, v2)
+	}
+
+	learnValue := learns[0].chosen()
+	if learnValue != v1 {
+		t.Errorf("learnValue = %s,excepted %s", learnValue, v1)
+	}
+
+}
